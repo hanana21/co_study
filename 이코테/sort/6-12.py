454ba9f9874b86332b5c4e85f3cb50f0db04327a
hanana21 <134573117+hanana21@users.noreply.github.com>
2023-12-29 23:24:44 +0900

    Add files via upload

--- 이코테/sort/6-12.py
+++ 이코테/sort/6-12.py
@@ -1,12 +1,17 @@
+# 문제: 두배열의 원소를 K번 바꿔치기 가능함, A의 모든 원소의 합이 최대가 되게 해준다.
+# 해결 방법: A의 최소값과 B의 최대값을 바꾸기
+
 n,k = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
-
 a.sort()
-b.sort()
+b.sort(reverse=True)
 for i in range(k):
-    if a[i] < b[i]:
-        a[i],b[i] =  b[i],a[i]
-    else:
+    if a[i] < b[i]: 
+        a[i] = b[i]
+    else: # 원소의 크기가 같거나 클때는 더이상 바꿀필요 없으니(이미 정렬된 상태) 걍 탈출
         break
 print(sum(a))
+
+
+
